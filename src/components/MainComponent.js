51bcc2005f1db404c390e7577c0cc000865c57df
manaljain6667 <manaljain6667@gmail.com>
2020-07-14 02:40:41 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
 import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state={
       dishes : DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
       //selectedDish:null
     };
   }
@@ -20,6 +27,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -31,6 +41,7 @@ class Main extends Component {
               {/* When you have a URL ending with /home, then this will route me to this particular component 
                   that is going to act as the view here. And the components name would be HomePage. */}
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
               {/* exact here that means that the path should exactly match this with nothing else, beyond menu */}
               <Redirect to="/home" />
               {/* if the URL part does not match anything then you will redirect it to the home component. */}
