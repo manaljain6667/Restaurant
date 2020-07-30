169417e6fb95a339de50407b608c510fd38f90e9
manaljain6667 <manaljain6667@gmail.com>
2020-07-30 17:54:58 +0530

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -16,9 +16,23 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
     return fetch(baseUrl + 'dishes')
+    .then(response => {
+        if (response.ok) {
+          return response;                 //if the response is ok then this response will be available to next then
+        } 
+        else { 
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);   //response.status gives the status code like 404  
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {                   //error handler incase if server does not even respond
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));
-     //dishes are obtained, then it'll push the dishes into the redux store here by dispatching that to the dishes.
+    .then(dishes => dispatch(addDishes(dishes))) //dishes are obtained, then it'll push the dishes into the redux store here by dispatching that to the dishes.
+    .catch(error => dispatch(dishesFailed(error.message)));
 }
 //The fetchDishes as you see is a function which returns a function which when called will try to fetch the dishes first by setting the isLoading to 
 //"True", and so that the spinner can be displayed and after a period of time the dishes are fetched and then added into the application.
@@ -31,12 +45,29 @@ export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
 });
-
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED, 
+    payload: errmess
+});
 //upper two functions returns functions
 export const fetchComments = () => (dispatch) => {    
     return fetch(baseUrl + 'comments')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
@@ -54,8 +85,22 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
 }
 
 export const promosLoading = () => ({
